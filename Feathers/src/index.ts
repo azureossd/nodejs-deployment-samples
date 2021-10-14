b720443f2e5e968a180b87c8b5387d07cea3b440
Anthony Salemo <ajssalemo@gmail.com>
2021-10-13 20:56:30 -0400

    putting a placeholder under /config/production.json

--- Feathers/src/index.ts
+++ Feathers/src/index.ts
@@ -1,7 +1,7 @@
 import logger from './logger';
 import app from './app';
 
-const port = process.env.PORT || 3033;
+const port = process.env.PORT || 3030;
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
